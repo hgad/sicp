2a0005dcac0a12037e25185d1ae6538487e1d827
Haitham Gad <haithamgad@gmail.com>
2015-02-08 16:02:23 -0500

    add some solutions for chapter 2

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # sicp
-Structure and Interpretation of Computer Programs exercise solutions.
+Structure and Interpretation of Computer Programs exercise solutions. All
+examples were tested using the MIT Scheme interpreter.
--- ch1.scm
+++ ch1.scm
@@ -424,6 +424,9 @@
 ;; ex1.21                                                                     ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(define (divides? a b)
+  (= (remainder b a) 0))
+
 (define (find-divisor n test-divisor)
   (cond ((> (square test-divisor) n) n)
         ((divides? test-divisor n) test-divisor)
@@ -431,9 +434,6 @@
 
 (define (smallest-divisor n)
   (find-divisor n 2))
-
-(define (divides? a b)
-  (= (remainder b a) 0))
 
 (smallest-divisor 199)   ; 199
 (smallest-divisor 1999)  ; 1999
