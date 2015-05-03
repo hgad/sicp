440e4965ecce4a4ab2cdfa1eda7156eb8ca631da
Haitham Gad <haithamgad@gmail.com>
2015-05-02 20:45:24 -0400

    finishes section 3.3

--- ch3.scm
+++ ch3.scm
@@ -2047,3 +2047,346 @@
 ; creating some part of the circuit, the action procedures for these parts will
 ; never be executed, because they will never have been added to the agenda.
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ex3.32 (incomplete)                                                        ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+; With queue:
+;   Wire = a Time = 0 Value = 1
+;   Wire = b Time = 0 Value = 0
+;   Wire = o Time = 0 Value = 0
+;   Wire = o Time = 2 Value = 1
+;   Wire = o Time = 2 Value = 0
+;
+; With stack:
+;   Wire = a Time = 0 Value = 1
+;   Wire = b Time = 0 Value = 0
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ex3.33                                                                     ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(module ex3.33 (make-connector has-value? get-value set-value! forget-value!
+                connect inform-about-value inform-about-no-value adder
+                multiplier constant probe averager)
+  (import scheme debug)
+  (import (only chicken error))
+  (title "ex3.33")
+
+  (define (for-each-except exception procedure list)
+    (define (loop items)
+      (cond ((null? items) 'done)
+            ((eq? (car items) exception) (loop (cdr items)))
+            (else (procedure (car items))
+                  (loop (cdr items)))))
+    (loop list))
+
+  (define (make-connector)
+    (let ((value #f) (informant #f) (constraints '()))
+      (define (set-my-value newval setter)
+        (cond ((not (has-value? me))
+               (set! value newval)
+               (set! informant setter)
+               (for-each-except setter
+                                inform-about-value
+                                constraints))
+              ((not (= value newval))
+               (error "Contradiction" (list value newval)))
+              (else 'ignored)))
+
+      (define (forget-my-value retractor)
+        (if (eq? retractor informant)
+            (begin (set! informant #f)
+                   (for-each-except retractor
+                                    inform-about-no-value
+                                    constraints))
+            'ignored))
+
+      (define (connect new-constraint)
+        (if (not (memq new-constraint constraints))
+            (set! constraints 
+                  (cons new-constraint constraints)))
+        (if (has-value? me)
+            (inform-about-value new-constraint))
+        'done)
+
+      (define (me request)
+        (cond ((eq? request 'has-value?)
+               (if informant #t #f))
+              ((eq? request 'value) value)
+              ((eq? request 'set-value!) set-my-value)
+              ((eq? request 'forget) forget-my-value)
+              ((eq? request 'connect) connect)
+              (else (error "Unknown operation -- CONNECTOR"
+                           request))))
+      me))
+
+  (define (has-value? connector)
+    (connector 'has-value?))
+
+  (define (get-value connector)
+    (connector 'value))
+
+  (define (set-value! connector new-value informant)
+    ((connector 'set-value!) new-value informant))
+
+  (define (forget-value! connector retractor)
+    ((connector 'forget) retractor))
+
+  (define (connect connector new-constraint)
+    ((connector 'connect) new-constraint))
+
+  (define (inform-about-value constraint)
+    (constraint 'I-have-a-value))
+
+  (define (inform-about-no-value constraint)
+    (constraint 'I-lost-my-value))
+
+  (define (adder a1 a2 sum)
+    (define (process-new-value)
+      (cond ((and (has-value? a1) (has-value? a2))
+             (set-value! sum
+                         (+ (get-value a1) (get-value a2))
+                         me))
+            ((and (has-value? a1) (has-value? sum))
+             (set-value! a2
+                         (- (get-value sum) (get-value a1))
+                         me))
+            ((and (has-value? a2) (has-value? sum))
+             (set-value! a1
+                         (- (get-value sum) (get-value a2))
+                         me))))
+
+    (define (process-forget-value)
+      (forget-value! sum me)
+      (forget-value! a1 me)
+      (forget-value! a2 me)
+      (process-new-value))
+
+    (define (me request)
+      (cond ((eq? request 'I-have-a-value)  
+             (process-new-value))
+            ((eq? request 'I-lost-my-value) 
+             (process-forget-value))
+            (else 
+             (error "Unknown request -- ADDER" request))))
+
+    (connect a1 me)
+    (connect a2 me)
+    (connect sum me)
+    me)
+
+  (define (multiplier m1 m2 product)
+    (define (process-new-value)
+      (cond ((or (and (has-value? m1) (= (get-value m1) 0))
+                 (and (has-value? m2) (= (get-value m2) 0)))
+             (set-value! product 0 me))
+            ((and (has-value? m1) (has-value? m2))
+             (set-value! product
+                         (* (get-value m1) (get-value m2))
+                         me))
+            ((and (has-value? product) (has-value? m1))
+             (set-value! m2
+                         (/ (get-value product) (get-value m1))
+                         me))
+            ((and (has-value? product) (has-value? m2))
+             (set-value! m1
+                         (/ (get-value product) (get-value m2))
+                         me))))
+
+    (define (process-forget-value)
+      (forget-value! product me)
+      (forget-value! m1 me)
+      (forget-value! m2 me)
+      (process-new-value))
+
+    (define (me request)
+      (cond ((eq? request 'I-have-a-value)
+             (process-new-value))
+            ((eq? request 'I-lost-my-value)
+             (process-forget-value))
+            (else
+             (error "Unknown request -- MULTIPLIER" request))))
+
+    (connect m1 me)
+    (connect m2 me)
+    (connect product me)
+    me)
+
+  (define (constant value connector)
+    (define (me request)
+      (error "Unknown request -- CONSTANT" request))
+    (connect connector me)
+    (set-value! connector value me)
+    me)
+
+  (define (probe name connector)
+    (define (print-probe value)
+      (newline)
+      (display "Probe: ")
+      (display name)
+      (display " = ")
+      (display value))
+
+    (define (process-new-value)
+      (print-probe (get-value connector)))
+
+    (define (process-forget-value)
+      (print-probe "?"))
+
+    (define (me request)
+      (cond ((eq? request 'I-have-a-value)
+             (process-new-value))
+            ((eq? request 'I-lost-my-value)
+             (process-forget-value))
+            (else
+             (error "Unknown request -- PROBE" request))))
+    (connect connector me)
+    me)
+
+  (define (averager a b c)
+    (let ((x (make-connector))
+          (y (make-connector)))
+      (multiplier x c y)
+      (adder a b y)
+      (constant 2 x)
+      'ok))
+
+  (define A (make-connector))
+  (define B (make-connector))
+  (define C (make-connector))
+  (averager A B C)
+
+  (probe "A" A)
+  (probe "B" B)
+  (probe "C" C)
+
+  (set-value! A 5 'user)
+  (set-value! B 7 'user)
+  (forget-value! A 'user)
+  (set-value! C 5 'user)
+
+  (newline))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ex3.34                                                                     ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+; The problem with this is that the value of a cannot be deduced from the value
+; of b. The multiplier assumes that two values are available in order to compute
+; the third value, but when a has no value (or when the value of a is
+; forgotten), the multiplier will have only one value, namely, the output value
+; set by the user.
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ex3.35                                                                     ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(module ex3.35 ()
+  (import scheme debug ex3.33)
+  (import (only chicken error))
+  (title "ex3.35")
+
+  (define (squarer a b)
+    (define (process-new-value)
+      (cond
+        ((has-value? b)
+         (if (< (get-value b) 0)
+             (error "square less than 0 -- SQUARER" (get-value b))
+             (set-value! a (sqrt (get-value b)) me)))
+        ((has-value? a)
+         (set-value! b (* (get-value a) (get-value a)) me))))
+
+    (define (process-forget-value)
+      (forget-value! b me)
+      (forget-value! a me)
+      (process-new-value))
+
+    (define (me request)
+      (cond ((eq? request 'I-have-a-value)
+             (process-new-value))
+            ((eq? request 'I-lost-my-value)
+             (process-forget-value))
+            (else
+             (error "Unknown request -- SQUARER" request))))
+
+    (connect a me)
+    (connect b me)
+    me)
+
+  (define A (make-connector))
+  (define B (make-connector))
+  (squarer A B)
+
+  (probe "A" A)
+  (probe "B" B)
+
+  (set-value! A 5 'user)
+  (forget-value! A 'user)
+  (set-value! B 25 'user)
+
+  (newline))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ex3.36 (incomplete)                                                        ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+; Skipping
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ex3.37                                                                     ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(module ex3.37 ()
+  (import scheme debug ex3.33)
+  (import (only chicken error))
+  (title "ex3.37")
+
+  (define (c+ x y)
+    (let ((z (make-connector)))
+      (adder x y z)
+      z))
+
+  (define (c- x y)
+    (let ((z (make-connector)))
+      (adder z y x)
+      z))
+
+  (define (c* x y)
+    (let ((z (make-connector)))
+      (multiplier x y z)
+      z))
+
+  (define (c/ x y)
+    (let ((z (make-connector)))
+      (multiplier z y x)
+      z))
+
+  (define (cv v)
+    (let ((x (make-connector)))
+      (constant v x)
+      x))
+
+  (define (celsius-fahrenheit-converter x)
+    (c+ (c* (c/ (cv 9) (cv 5))
+            x)
+        (cv 32)))
+
+  (define C (make-connector))
+  (define F (celsius-fahrenheit-converter C))
+
+  (probe "C" C)
+  (probe "F" F)
+
+  (set-value! C 25 'user)
+  (forget-value! C 'user)
+  (set-value! F 212 'user)
+
+  (newline))
+
